fab16073f5fb8c5917c7a0cbd3eda20e469940c2
IsmailAkhrouf <ismail.akhrouf123@gmail.com>
2022-02-08 16:10:44 +0100

    Ismail Commit

--- src/main/java/com/example/api_project/controller/ArbitreController.java
+++ src/main/java/com/example/api_project/controller/ArbitreController.java
@@ -1,4 +1,31 @@
 package com.example.api_project.controller;
 
+import com.example.api_project.entity.Arbitre;
+import com.example.api_project.service.ArbitreService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/arbitres")
 public class ArbitreController {
+
+    @Autowired
+    ArbitreService arbitreService;
+
+    @GetMapping("/all")
+    public List<Arbitre> getAllArbitres(){
+        return arbitreService.getArbitre();
+    }
+
+    @PostMapping("/add")
+    public void addArbitre(@RequestBody Arbitre arbitre){
+        arbitreService.addAarbitre(arbitre);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public void deleteArbitre(@PathVariable Long id){
+        arbitreService.deleteArbitre(id);
+    }
 }
--- src/main/java/com/example/api_project/controller/MatcheController.java
+++ src/main/java/com/example/api_project/controller/MatcheController.java
@@ -1,4 +1,65 @@
 package com.example.api_project.controller;
 
+import com.example.api_project.entity.Equipe;
+import com.example.api_project.entity.Matche;
+import com.example.api_project.entity.Stade;
+import com.example.api_project.repositories.MatcheRepository;
+import com.example.api_project.service.MatchService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+@RestController
+@RequestMapping("/matches")
 public class MatcheController {
+
+    @Autowired
+    MatchService matchService;
+
+    @Autowired
+    MatcheRepository matcheRepository;
+
+    @GetMapping("/all")
+    public List<Matche> getAllMatches(){
+        return matchService.getMatches();
+    }
+
+    @PostMapping("/add")
+    public void saveMatche(@RequestBody Matche matche){
+        matchService.addMatche(matche);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public void deleteMatcheById(@PathVariable Long id){
+        matchService.deleteMatche(id);
+    }
+
+    @GetMapping("/find/{date}")
+    public List<Matche> getMatchesBydate(@PathVariable String date) throws ParseException {
+        String pattern = "yyyy-MM-dd";
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
+        Date datematch = simpleDateFormat.parse(date);
+        return matcheRepository.findMatchesBydate(datematch);
+    }
+
+    @GetMapping("/findStateById/{id}")
+    public Stade getStadeByMatchId(@PathVariable Long id){
+        return matcheRepository.findStadeByMatchID(id);
+    }
+
+    @GetMapping("/findEquipesById/{id}")
+    public List<Equipe> getEquipesByMatchID(@PathVariable Long id){
+        return matcheRepository.findEquipesByMatchID(id);
+    }
+
+    @DeleteMapping("/deletePasses")
+    public void supprimeMatchesPasse()  {
+        matcheRepository.suppmatchpasse();
+    }
+
+
 }
