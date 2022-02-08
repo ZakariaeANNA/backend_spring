afe78a03c38fa63fa8af183dd962ce4d6520ec26
Moaad123 <moaadakdi123@gmail.com>
2022-02-08 16:38:42 +0100

    moaad commit

--- src/main/java/com/example/api_project/controller/ArbitreController.java
+++ src/main/java/com/example/api_project/controller/ArbitreController.java
@@ -3,6 +3,8 @@ package com.example.api_project.controller;
 import com.example.api_project.entity.Arbitre;
 import com.example.api_project.service.ArbitreService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,17 +17,23 @@ public class ArbitreController {
     ArbitreService arbitreService;
 
     @GetMapping("/all")
-    public List<Arbitre> getAllArbitres(){
-        return arbitreService.getArbitre();
+    public ResponseEntity<List<Arbitre>> getAllArbitres()
+    {
+        List<Arbitre> arbitres = arbitreService.getArbitre();
+        return new ResponseEntity<>(arbitres, HttpStatus.OK);
     }
 
     @PostMapping("/add")
-    public void addArbitre(@RequestBody Arbitre arbitre){
+    public ResponseEntity<?> addArbitre(@RequestBody Arbitre arbitre)
+    {
         arbitreService.addAarbitre(arbitre);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @DeleteMapping("/delete/{id}")
-    public void deleteArbitre(@PathVariable Long id){
+    public ResponseEntity<?> deleteArbitre(@PathVariable Long id)
+    {
         arbitreService.deleteArbitre(id);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
