497152972c3e5984eb229ab35157f52e2556eca6
Moaad123 <moaadakdi123@gmail.com>
2022-02-08 15:58:01 +0100

    moaad commit

---
+++ src/main/java/com/example/api_project/controller/JoueurController.java
@@ -0,0 +1,4 @@
+package com.example.api_project.controller;
+
+public class JoueurController {
+}
--- src/main/java/com/example/api_project/service/EquipeService.java
+++ src/main/java/com/example/api_project/service/EquipeService.java
@@ -5,6 +5,7 @@ import com.example.api_project.entity.Equipe;
 import com.example.api_project.repositories.EquipeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -26,5 +27,14 @@ public class EquipeService {
     public void deletequipe(Long id)
     {
         equipeRepository.deleteById(id);
+    }
+
+    public List<Equipe> deleteEquipebypays(String pays){
+        List<Equipe> equipes = equipeRepository.findAll();
+        equipes.forEach((equipe -> {
+            if(!(equipe.getPays().equals(pays)))
+                System.out.println(equipe.getNomEquipe());
+        }));
+        return equipes;
     }
 }
