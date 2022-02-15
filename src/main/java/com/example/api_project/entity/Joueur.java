3d846640ebe4413cefe4b5381ecd9c46dfc5b97b
Zakariae Anna <61186326+ZakariaeANNA@users.noreply.github.com>
2022-02-15 16:11:40 +0100

    Update Joueur.java

--- src/main/java/com/example/api_project/entity/Joueur.java
+++ src/main/java/com/example/api_project/entity/Joueur.java
@@ -2,12 +2,9 @@ package com.example.api_project.entity;
 
 
 import com.fasterxml.jackson.annotation.*;
-<<<<<<< HEAD
-=======
 import com.fasterxml.jackson.databind.ser.FilterProvider;
 import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
 import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
->>>>>>> d2a82b147b1c6b43291831c421d3b5bbae46047a
 import lombok.Data;
 
 import javax.persistence.*;
@@ -15,7 +12,6 @@ import java.io.Serializable;
 
 @Entity
 @Data
-@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class,property = "idJoueur")
 public class Joueur{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -26,21 +22,16 @@ public class Joueur{
 
     @Column
     String poste;
-<<<<<<< HEAD
+
     @ManyToOne(fetch = FetchType.EAGER,cascade = CascadeType.MERGE)
     @JoinColumn(name="id_Equipe")
     @JsonBackReference("equipe")
-=======
 
 
     @JsonBackReference("equipe")
     @ManyToOne(fetch = FetchType.EAGER,cascade = CascadeType.MERGE)
     @JoinColumn(name="idEquipe")
-<<<<<<< HEAD
-=======
     @JsonIgnoreProperties({"joueurs","matches"})
->>>>>>> d2a82b147b1c6b43291831c421d3b5bbae46047a
->>>>>>> 7bc3de2c00b3ffc91cfcde9415cfd72c27412ef8
     Equipe equipe;
     
 }
